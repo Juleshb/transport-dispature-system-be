f504b71682aef5dc6656dd58680d1823c7c9ecdc
Juleshb <habaruremajules@gmail.com>
2022-08-19 01:16:52 +0200

    SystemUsers

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::GET('/users',[SystemUsersController::class,'allusers']);
 
 
 Route::post("register",[UserController::class,'register']);
-Route::post("login",[UserController::class,'login']);
+//Route::post("login",[UserController::class,'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [UserController::class, 'user']);
