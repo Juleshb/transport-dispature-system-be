4aa3bbed7a10bb52dfd09328c6ad6f6d5e10ac22
Juleshb <habaruremajules@gmail.com>
2022-08-19 01:38:47 +0200

    SystemUsers

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+// use Illuminate\Support\Facades\Gate;
+use Laravel\Sanctum\Sanctum;
+use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
         //
     }
 }
