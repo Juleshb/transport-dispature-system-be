121eed3f394a7e8a8831c945bf2f2358d774fbf5
Juleshb <habaruremajules@gmail.com>
2022-08-19 01:54:58 +0200

    SystemUsers

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("list",[BlogAPI::class,'list']);
+
 
 //SUPER ADIMIN ROUTERS
 Route::POST('/superadminregister',[SystemUsersController::class,'store']);
