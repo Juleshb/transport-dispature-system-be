94d78c893d75f3f0b9ba9ac1d9eff027cc75d857
Juleshb <habaruremajules@gmail.com>
2022-08-19 10:20:19 +0200

    SystemUsers

--- app/Http/Controllers/AgenceController.php
+++ app/Http/Controllers/AgenceController.php
@@ -33,7 +33,7 @@ class AgenceController extends Controller
           'name' => $request->company_name,
           'email' => $request->company_Email,
           'password' => Hash::make($request->password),
-          'role'=> $request->role,
+          'role'=> 2
       ]);
      
      return response([
--- app/Models/Agence.php
+++ app/Models/Agence.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Symfony\Component\HttpKernel\Profiler\Profile;
+// use Symfony\Component\HttpKernel\Profiler\Profile;
 
 
 
@@ -18,8 +18,8 @@ class Agence extends Authenticatable
     protected $fillable=['company_name','company_Admin','company_Code',
     'company_OwnershipType','company_Email'];
 
-    public function profile(){
-        return $this->hasOne(Profile::class);
-    }
+    // public function profile(){
+    //     return $this->hasOne(Profile::class);
+    // }
    
 }
