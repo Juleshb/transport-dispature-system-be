1bb5a7812a8dc206daaf4b45203465da69cf12d7
Juleshb <habaruremajules@gmail.com>
2022-08-19 02:51:30 +0200

    SystemUsers

--- app/Models/Agence.php
+++ app/Models/Agence.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-// use App\Models\Reciptionist;
+
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
@@ -14,7 +14,7 @@ class Agence extends Authenticatable
     use HasFactory, HasApiTokens,Notifiable;
     public $timestamps=false;
     protected $fillable=['company_name','company_Admin','company_Code',
-    'password','company_OwnershipType','created_at','role','updated_at'];
+    'password','company_OwnershipType','password','role'];
 
     // public function Doctor(){
     // return $this->hasMany(Doctor::class);
