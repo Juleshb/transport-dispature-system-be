96efa2440944556d364b0658e244808520c12cbc
Jules Habarurema <habaruremajules@gmail.com>
2022-09-15 12:52:40 +0200

    modify roles

--- app/Http/Controllers/bktichet.php
+++ app/Http/Controllers/bktichet.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\BKticket;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\BKticket;
+use Illuminate\Support\Facades\DB;
 
 class bktichet extends Controller
 {
@@ -12,10 +12,9 @@ class bktichet extends Controller
         if(auth()->user()->role== 3){
         $request->validate(
         [
-                'company_id' => 'required'
-              
+            'company_id' => 'required'
                 
-            ]
+        ]
         );
         BKticket::create([
             'company_id'=>$request->input('company_id'),
@@ -26,22 +25,34 @@ class bktichet extends Controller
         );
 
         return response([
-            'results'=>'tichet created successfully'
+            'results'=>'Router created successfully'
           ]);}
           else{
             return response(['message'=>'you are not allowed to perform this action']);
         }
     }
-    
      //show all routers
-     
-
-       public function showAll(){
-        if(auth()->user()->role== 3){
-            return BKticket::all();
+     public function showAll(){
+        if(auth()->user()->role=='3'){
+     return response([
+        'buss list'=>Router::where('agence_id',auth()->user()->id)->get()
+     ]);
     }
     else{
     return response([
         'message'=>'you are not allowed'
     ]);
-    }}}
+    }}
+
+       public function showrout(){
+        // if(auth()->user()->role=='2'){
+            $router =DB::table('routers')
+            ->join('users', 'users.id', '=', 'routers.agence_id')
+            ->select('users.name', 'routers.router_name','routers.amount')
+            ->get();
+     return response([
+        'Router list'=>$router
+     ]);
+    // 
+}
+}
